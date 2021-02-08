f3df962ac11b138557ef66bc3e75b3efaa964aac
Alex Chubaty <achubaty@for-cast.ca>
2021-02-08 09:07:10 -0700

    update B_bDP and set reproducible.polygonShortcut=FALSE

--- 04-options.R
+++ 04-options.R
@@ -41,6 +41,7 @@ opts <- options(
   "reproducible.inputPaths" = if (user("emcintir")) normPath("~/data") else NULL,
   "reproducible.nThreads" = 2,
   "reproducible.overwrite" = TRUE,
+  "reproducible.polygonShortcut" = FALSE,
   "reproducible.quick" = FALSE,
   "reproducible.showSimilar" = TRUE,
   "reproducible.useCache" = if (peutils::user("emcintir")) TRUE else TRUE,
