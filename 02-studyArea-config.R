aa3db6006e7d98b10495ca3b306db175f5e2b499
Ian Eddy <ianmseddy@gmail.com>
2023-02-07 08:48:27 -0800

    dont use NFDB for age in ROF

--- 02-studyArea-config.R
+++ 02-studyArea-config.R
@@ -19,7 +19,9 @@ config.studyArea <- list(
     Biomass_borealDataPrep = list(
       speciesTableAreas = c("WestON"),
       subsetDataAgeModel = 50,
-      subsetDataBiomassModel = 50
+      subsetDataBiomassModel = 50,
+      overrideAgeInFires = if (grepl("^ON_ROF", config$context[["studyAreaName"]])) FALSE else TRUE,
+      overrideBiomassInFires = if(grepl("^ON_ROF", config$context[["studyAreaName"]])) FALSE else TRUE
     ),
     Biomass_speciesData = list(
       types = if (grepl("ON", config$context[["studyAreaName"]])) {
