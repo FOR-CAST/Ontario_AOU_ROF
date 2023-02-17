6e8cec7432e8743e9acc7af43a86e6ab8f7c2579
Alex Chubaty <achubaty@for-cast.ca>
2023-02-17 08:37:22 -0700

    update gids

--- 00-global.R
+++ 00-global.R
@@ -99,7 +99,7 @@ library(Require)
 setLinuxBinaryRepo()
 
 Require(c(
-  "PredictiveEcology/SpaDES.project@transition (>= 0.0.7.9003)", ## TODO: use development once merged
+  "PredictiveEcology/SpaDES.project@transition (>= 0.0.7.9018)", ## TODO: use development once merged
   "PredictiveEcology/SpaDES.config@development (>= 0.0.2.9066)"
 ), upgrade = FALSE, standAlone = TRUE)
 
