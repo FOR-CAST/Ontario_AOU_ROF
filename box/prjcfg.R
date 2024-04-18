c78bfd2adf368da1437ce554524e35c8a0c2ca60
Alex Chubaty <achubaty@for-cast.ca>
2024-04-18 15:05:00 -0600

    custom outputDir for canClimateData
    avoid duplicated data when replicating sims

--- box/prjcfg.R
+++ box/prjcfg.R
@@ -417,6 +417,7 @@ onnrvConfig <- R6::R6Class(
           climateGCM = self$context$climateGCM,
           climateSSP = self$context$climateSSP,
           historicalFireYears = 1971:2022, ## TODO: using more years for sampling
+          outputDir = file.path(dirname(self$paths$outputPath), "climate"), ## outputs/studyArea/climate
           projectedType = "forecast",
           studyAreaName = self$context$studyAreaName,
           .useCache = FALSE ## c(".inputObjects", "init")
