9c07332524988275c6d71cda7cabf738d9dc47f5
Alex Chubaty <achubaty@for-cast.ca>
2024-04-30 12:50:47 -0600

    spades.allowInitDuringSimInit = FALSE
    sim end time bug breaks some modules, and i don't believe we need this behaviour here

--- box/prjcfg.R
+++ box/prjcfg.R
@@ -326,7 +326,7 @@ onnrvConfig <- R6::R6Class(
         reproducible.useCloud = FALSE, ## TODO: cloudCache spams Google Drive; doesn't respect drive path
         reproducible.useTerra = TRUE,
         Require.install = FALSE, ## don't use Require; assume all pkgs installed
-        spades.allowInitDuringSimInit = TRUE,
+        spades.allowInitDuringSimInit = FALSE,
         spades.allowSequentialCaching = FALSE,
         spades.futurePlan = "callr",
         # spades.memoryUseInterval = 10, ## track memory use every 10 seconds
