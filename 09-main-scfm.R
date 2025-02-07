c33a5a961cc79ceee700f5a697d7a3ecad113edf
Alex Chubaty <achubaty@for-cast.ca>
2025-02-06 22:30:16 -0700

    don't use Erni et al. burn rates
    b/c they are lower than scfm's and trigger error

--- 09-main-scfm.R
+++ 09-main-scfm.R
@@ -29,9 +29,10 @@ loadorder_sim <- c(
 
 ## add scfmRegime targetBurnRate param based on FRP attrs from Erni et al.
 ## NOTE: Burn.rate is percent per year; we want proportion per year
-targetBurnRate <- simOutPreamble[["fireRegimePolysLarge"]][["Burn.rate"]] / 100
-names(targetBurnRate) <- simOutPreamble[["fireRegimePolysLarge"]][["PolyID"]]
-config$params[["scfmRegime"]][["targetBurnRate"]] <- targetBurnRate
+## NOTE: Erni et al. burn rates lower than scfm's
+# targetBurnRate <- simOutPreamble[["fireRegimePolysLarge"]][["Burn.rate"]] / 100
+# names(targetBurnRate) <- simOutPreamble[["fireRegimePolysLarge"]][["PolyID"]]
+# config$params[["scfmRegime"]][["targetBurnRate"]] <- targetBurnRate
 
 parameters_sim <- config$params
 
